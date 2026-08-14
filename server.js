b9b60c24a39edd594b83a8121a9fa741eef02b57
ninngenn0121 <ninngenn0121@outlook.jp>
2026-08-14 22:44:44 +0900

    Update server.js

--- server.js
+++ server.js
@@ -23,36 +23,3 @@ io.on('connection', (socket) => {
     io.emit('update user list', users);
   });
 
-  socket.on('join room', (roomName) => {
-    socket.join(roomName);
-  });
-
-  // 新規メッセージ受信
-  socket.on('chat message', (data) => {
-    if (data.targetRoom) {
-      io.to(data.targetRoom).emit('chat message', {
-        id: Date.now() + Math.random().toString(36).substr(2, 9), // ユニークID
-        user: users[socket.id] || '匿名',
-        text: data.text,
-        targetRoom: data.targetRoom,
-        time: new Date().toLocaleTimeString([], { hour: '2-digit', minute: '2-digit' }) // 現在時刻 (例: 14:30)
-      });
-    }
-  });
-
-  // 🌟 メッセージ編集の受信＆全員への配信
-  socket.on('edit message', (data) => {
-    if (data.targetRoom) {
-      io.to(data.targetRoom).emit('edit message', {
-        id: data.id,
-        newText: data.newText,
-        targetRoom: data.targetRoom
-      });
-    }
-  });
-
-  socket.on('disconnect', () => {
-    delete users[socket.id];
-    io.emit('update user list', users);
-  });
-});
